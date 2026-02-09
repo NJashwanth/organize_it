108cfd223036e7a67f86b9d8444f785908077ba3
JASHWANTH NEELA <jashwanthn066@gmail.com>
2026-02-08 23:24:13 -0500

    added comments for add_task.dart

--- lib/features/tasks/domain/usecases/add_task.dart
+++ lib/features/tasks/domain/usecases/add_task.dart
@@ -1,6 +1,7 @@
 import 'package:organize_it/features/tasks/domain/entities/task.dart';
 import 'package:organize_it/features/tasks/data/repositories/task_repository.dart';
 
+/// Domain-level use case for adding a task.
 class AddTaskUseCase {
   final TaskRepository repository;
 
